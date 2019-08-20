9a79428873eaa1cc744d3350178a2772f31402ba
Karina Kalpaxis <kalpaxis1@gmail.com>
2019-08-20 17:28:59 -0400

    Recitation Review code

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,63 @@
-// Test away
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react/pure';
+import Dashboard from './Dashboard.js';
+
+
+describe('<Dashboard />', () => {
+  beforeEach(cleanup);
+  it('renders without crashing', () => {
+    render(<Dashboard />);
+  });
+});
+
+describe('<Dashboard /> state transitions', () => {
+  const { getByText } = render(<Dashboard />);
+  it('default state open and unlocked', () => {
+    // verify open and unlocked
+    getByText(/^open$/i);
+    getByText(/^unlocked$/i);
+
+    getByText(/^lock gate$/i);
+    getByText(/^close gate$/i);
+  });
+  it('open and unlocked to closed and unlocked', () => {
+    const closeBtn = getByText(/^close gate$/i);
+    fireEvent.click(closeBtn);
+    // check display text
+    getByText(/^closed$/i);
+    getByText(/^unlocked$/i);
+    // check button text
+    getByText(/^lock gate$/i);
+    getByText(/^open gate$/i);
+  });
+  it('closed and unlocked to closed and locked', () => {
+    const lockBtn = getByText(/^lock gate$/i);
+    fireEvent.click(lockBtn);
+    // check display text
+    getByText(/^closed$/i);
+    getByText(/^locked$/i);
+    // check button text
+    getByText(/^unlock gate$/i);
+    getByText(/^open gate$/i);
+  });
+  it('closed and locked to closed and unlocked', () => {
+    const lockBtn = getByText(/^unlock gate$/i);
+    fireEvent.click(lockBtn);
+    // check display text
+    getByText(/^closed$/i);
+    getByText(/^unlocked$/i);
+    // check button text
+    getByText(/^lock gate$/i);
+    getByText(/^open gate$/i);
+  });
+  it('closed and unlocked to open and unlocked', () => {
+    const openBtn = getByText(/^open gate$/i);
+    fireEvent.click(openBtn);
+    // check display text
+    getByText(/^open$/i);
+    getByText(/^unlocked$/i);
+    // check button text
+    getByText(/^lock gate$/i);
+    getByText(/^close gate$/i);
+  });
+});
